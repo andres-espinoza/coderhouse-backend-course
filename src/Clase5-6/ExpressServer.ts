0c5cd5f27426d06408fd7a8a888f4dcbdb5d0466
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2022-09-02 20:11:32 -0400

    Apuntes clase

---
+++ src/Clase5-6/ExpressServer.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+//* construyendo algunos endpoints tipo get
+
+app.get('/', (_req, res) => res.send(
+  '<h1 style="color: violet">Welcome to the Express Server</h1>'
+));
+
+app.get('/api', (_req, res) => res.send('Welcome to our API'));
+
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening port: ${PORT}`);
+  
+})
+
+server.on('error', (error) => console.error('Error: ', error?.message));
---
+++ src/Clase5-6/HTTPserver.ts
@@ -0,0 +1,22 @@
+import * as http from 'http';
+
+// Un mensaje de saludo según el rango de hora
+const message = (): string => {
+  const hour = new Date().getHours();
+  if(hour >= 6 && hour <= 12) return 'Good Morning';
+  if(hour >= 13 && hour <= 19) return 'Good Evening';
+  return 'Good Night';
+};
+
+// Se crea el servidor web
+const app = http.createServer((_req, res) => {
+  res.end(message());
+});
+
+// definir un puerto
+const PORT = 8082;
+
+// escuchando los cambios en el puerto ya definido
+app.listen(PORT, () => {
+  console.log(`Escuchando al puerto ${PORT}`);
+});
