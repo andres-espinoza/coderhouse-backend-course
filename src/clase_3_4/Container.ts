4dd9a8df2b5d97caf97e3268787fe1212e27980b
Andrés Espinoza D <andres.espinozad@mail.udp.cl>
2022-09-08 23:01:10 -0400

    refactorizando el desafío de la clase 3 y 4

--- src/clase_3_4/Container.ts
+++ src/clase_3_4/Container.ts
@@ -1,29 +1,6 @@
+import { IProduct, masaiYellow, mastia, python } from './sample';
+
 const fs = require('fs');
-
-export interface IProduct {
-  title: string;
-  price: number;
-  thumbnail: string;
-  id?: number;
-}
-
-export const mastia : IProduct = {
-  title: 'Zapatilla Escalada Mastia',
-  price: 123900,
-  thumbnail: 'https://cdnx.jumpseller.com/tienda-ruta-outdoor/image/7200691/resize/480/480?1657573387 1x,https://cdnx.jumpseller.com/tienda-ruta-outdoor/image/7200691/resize/960/960?1657573387 2x'
-}
-
-export const masaiYellow: IProduct = {
-  title: 'Zapatilla Escalada Masai Yellow',
-  price: 81900,
-  thumbnail: 'https://cdnx.jumpseller.com/tienda-ruta-outdoor/image/7200677/resize/480/480?1657573189'
-}
-
-const python: IProduct = {
-  title: 'Zapatilla Escalada Python',
-  price: 90990,
-  thumbnail: 'https://shop.epictv.com/en/climbing-shoes/la-sportiva/python-2015'
-}
 
 class Container {
   
